9f5b0c3d7f36e7de9e0e6190d0e1fdadbbea6c8b
SaitoAmei <oleg.prusak2004@gmail.com>
2022-04-29 00:33:41 +0300

    try/catch get all/ pagination

--- Controllers/DataAllController.cs
+++ Controllers/DataAllController.cs
@@ -8,6 +8,7 @@ using Job_test_task_Announcements_Api.Models;
 using System.Threading.Tasks;
 using System.Net;
 using System.Collections.Generic;
+using System;
 
 namespace Job_test_task_Announcements_Api.Controllers
 {
@@ -18,27 +19,42 @@ namespace Job_test_task_Announcements_Api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllData(string order_by, bool reverse)
         {
-            List<Announcement> data = new();
-            await DbCreation.Creating_Db();
-            StringBuilder dat = new("");
-            if (order_by == "price")
+            try
             {
+                List<Announcement> data = new();
+                await DbCreation.Creating_Db();
+                StringBuilder dat = new("");
+                if (order_by == "price")
+                {
 
-                data = Queries.Get_Data("price", reverse, DbCreation.Connection()).Result;
-                
-            }
-            else if (order_by == "date")
-            {
-                data = Queries.Get_Data("date", reverse, DbCreation.Connection()).Result;
-            }
-            else { data = Queries.Get_Data("", reverse, DbCreation.Connection()).Result; }
-            
+                    data = Queries.Get_Data("price", reverse, DbCreation.Connection()).Result;
 
-            foreach(var i in data)
-            {
-                dat.Append(JsonSerializer.Serialize(i));
+                }
+                else if (order_by == "date")
+                {
+                    data = Queries.Get_Data("date", reverse, DbCreation.Connection()).Result;
+                }
+                else { data = Queries.Get_Data("", reverse, DbCreation.Connection()).Result; }
+
+                Page page = new() { Pag = 1, Elements = 10 };
+                foreach (var i in data)
+                {
+                    if (page.Elements == 10)
+                    {
+                        dat.Append(JsonSerializer.Serialize(page));
+                        page.Pag++;
+                        page.Elements = 0;
+                    }
+
+                    dat.Append(JsonSerializer.Serialize(new { i.Title, i.MainFotoLink, i.Price }));
+                    /*dat.Append(JsonSerializer.Serialize(i));*/
+                    page.Elements++;
+
+                }
+                return Ok(dat.ToString()) ;
             }
-            return Ok(dat.ToString());
+            catch (Exception ) { return Conflict(); }
+
 
 
         }
