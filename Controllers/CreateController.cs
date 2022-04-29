c618c7ee146960ea0f7b83df4a9a1d5f808b82d3
SaitoAmei <oleg.prusak2004@gmail.com>
2022-04-29 09:32:56 +0300

    full try_catch in creation _ id return  custom request created

--- Controllers/CreateController.cs
+++ Controllers/CreateController.cs
@@ -1,6 +1,7 @@
 ﻿using Job_test_task_Announcements_Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Text.Json;
 
 using System.Threading.Tasks;
 
@@ -21,18 +22,21 @@ namespace Job_test_task_Announcements_Api.Controllers
                 string addfoto1_s = addfoto1;
                 string addfoto2_s = addfoto2;
 
-                if (title == null || mainfoto == null) { throw new ArgumentException(); }
+                if (title == null || mainfoto == null || price == 0) { throw new ArgumentException(); }
                 if (description == null || description == "") { description_s = new("Not"); }
-                if (addfoto1 == null || addfoto1 == "") {  addfoto1_s = new("Not"); }
+                if (addfoto1 == null || addfoto1 == "") { addfoto1_s = new("Not"); }
                 if (addfoto2 == null || addfoto2 == "") { addfoto2_s = new("Not"); }
-                if ( title.Length > 200 || description_s.Length > 100 ) { throw new ArgumentException(); }
+                if (title.Length > 200 || description_s.Length > 100) { throw new ArgumentException(); }
                 await Queries.Create(new Announcement(title, description_s, price, mainfoto, addfoto1_s, addfoto2_s, DateTime.Now));
 
-                return Ok();
+                return Ok(new RequestCustom() { Id = (Queries.GetElementCount().Result + 1).ToString(), Status = Ok().StatusCode.ToString(), Description = "Success" });
             }
-            catch (ArgumentException) { return BadRequest(); }
-            //catch (ArgumentNullException) { return BadRequest(); }
-            //catch (Exception ) { return BadRequest(); }
+            catch (ArgumentException)
+            {
+                return BadRequest(JsonSerializer.Serialize(new RequestCustom() { Id = (Queries.GetElementCount().Result + 1).ToString(), Status = BadRequest().StatusCode.ToString(), Description = " Title and MainfotoLink fields can`t be empty !!!" }));
+            }
+
+            catch (Exception) { return BadRequest(JsonSerializer.Serialize(new RequestCustom() { Id = (Queries.GetElementCount().Result + 1).ToString(), Status = BadRequest().StatusCode.ToString(), Description = " Undefined exception !!!" })); }
         }
     }
 }
---
+++ Models/RequestCustom.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Job_test_task_Announcements_Api.Models
+{
+    public class RequestCustom
+    {
+        public string Id { get; set; }
+
+        public string Status{ get; set; }
+        public string Description { get; set; }
+    }
+}
